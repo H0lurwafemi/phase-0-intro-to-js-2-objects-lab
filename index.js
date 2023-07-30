ba398ca90d802ea46fbeab91e507f7fdf87d82e6
Holurfemi <stephenoluwafemi52@gmail.com>
2023-07-30 03:55:23 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,68 @@
 // Write your solution in this file!
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const updatedEmployee = { ...employee };
+  
+    updatedEmployee[key] = value;
+  
+    return updatedEmployee;
+  }
+  
+  // Example usage:
+  let employee = {
+    name: "John",
+    age: 30,
+    department: "HR",
+  };
+  
+  const updatedEmployee = updateEmployeeWithKeyAndValue(employee, "position", "Manager");
+  
+  console.log(updatedEmployee);
+  
+  console.log(employee);
+
+
+  
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  // Update the employee object with the new key-value pair
+  employee[key] = value;
+
+  // Return the entire updated employee object
+  return employee;
+}
+
+
+destructivelyUpdateEmployeeWithKeyAndValue(employee, "position", "Manager");
+
+console.log(employee);
+
+
+function deleteFromEmployeeByKey(employee, key) {
+    const updatedEmployee = { ...employee };
+  
+    delete updatedEmployee[key];
+  
+    return updatedEmployee;
+  }
+  
+ 
+  
+  const updatedEmployee1 = deleteFromEmployeeByKey(employee, "age");
+  
+  console.log(updatedEmployee);
+  
+  console.log(employee);
+  
+
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+  
+    return employee;
+  }
+  
+
+  
+  destructivelyDeleteFromEmployeeByKey(employee, "age");
+  
+  console.log(employee);
+  // Output: { name: "John", department: "HR" } (modified employee object without the deleted key/value pair)
+
